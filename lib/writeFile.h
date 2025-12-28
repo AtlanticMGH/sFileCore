530ae4d126cf18ec9e02c2db3dca86c6f03c4cfe
AtlanticMGH <90004815+AtlanticMGH@users.noreply.github.com>
2025-12-28 13:58:29 +0100

    Delete lib/writeFile.h

--- lib/writeFile.h
+++
@@ -1,9 +0,0 @@
-#ifndef WRITEFILE_H
-#define WRITEFILE_H
-
-void overWriteFile(char fileName[], char fileContent[]);
-void appendFile(char fileName[], char key[], float value);
-void deleteEntry(char fileName[], char key[]);
-void updateEntry(char fileName[], char key[], float value);
-
-#endif
